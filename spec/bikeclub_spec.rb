7293e44c0a769786cf4ddfa2bdcdaa22c83bfefc
Anna Wiley <awiley33@gmail.com>
2023-06-05 12:13:36 -0500

    "test: Add test for finding biker with most rides"

--- spec/bikeclub_spec.rb
+++ spec/bikeclub_spec.rb
@@ -5,6 +5,8 @@ describe BikeClub do
     @club1 = BikeClub.new("Cali Cruisers")
     @biker1 = Biker.new("Kenny", 30)
     @biker2 = Biker.new("Athena", 15)
+    @ride1 = Ride.new({name: "Walnut Creek Trail", distance: 10.7, loop: false, terrain: :hills})
+    @ride2 = Ride.new({name: "Town Lake", distance: 14.9, loop: true, terrain: :gravel})
   end
 
   it "exists and has a name" do
@@ -20,4 +22,26 @@ describe BikeClub do
 
     expect(@club1.bikers).to eq([@biker1, @biker2])
   end
+
+  it "can tell us which Biker has logged the most rides" do
+    @club1.add_biker(@biker1)
+    @club1.add_biker(@biker2)
+    @biker1.learn_terrain!(:gravel)
+    @biker1.learn_terrain!(:hills)
+    @biker2.learn_terrain!(:gravel)
+    @biker2.learn_terrain!(:hills)
+    @biker1.log_ride(@ride1, 92.5)
+    @biker1.log_ride(@ride2, 61.6)
+    @biker2.log_ride(@ride2, 65.0)
+
+    expect(@club1.most_rides).to eq(@biker1)
+  end
+
+  it "can tell us which Bikers are eligible for a given Ride. A Biker is eligible for a Ride if the terrain is acceptable to them and the total distance does not exceed their max distance" do
+
+  end
+
+  # it "can tell us which Biker has the best time for a given Ride" do
+
+  # end
 end
