453cf102403b37e1529a272716e08120e542bc6d
Anna Wiley <awiley33@gmail.com>
2023-06-05 10:29:07 -0500

    feat: Create Biker class and add attr reader, initialize method

---
+++ lib/biker.rb
@@ -0,0 +1,11 @@
+class Biker
+  attr_reader :name,
+              :max_distance
+          
+  def initialize(name, max_distance)
+    @name = name
+    @max_distance = max_distance
+    @rides = []
+    @acceptable_terrain = []
+  end
+end
