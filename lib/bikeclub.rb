f3c2a62577abf810007f802e2fb74d533cf4ed9d
Anna Wiley <awiley33@gmail.com>
2023-06-05 12:32:17 -0500

    feat: Add bikers eligible method

--- lib/bikeclub.rb
+++ lib/bikeclub.rb
@@ -14,4 +14,14 @@ class BikeClub
   def most_rides
     @bikers.max_by { |biker| biker.total_rides }
   end
+
+  def bikers_eligible(ride)
+    eligible = []
+    @bikers.each do |biker|
+      if biker.acceptable_terrain.include?(ride.terrain) && biker.max_distance >= ride.total_distance
+        eligible << biker
+      end
+    end
+    eligible
+  end
 end
