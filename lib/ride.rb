89b6e12e55bd61da5e43540eb29dbc7be78635a8
Anna Wiley <awiley33@gmail.com>
2023-06-05 10:55:19 -0500

    feat: Add helper method logging times in array associated w specific ride

--- lib/ride.rb
+++ lib/ride.rb
@@ -1,13 +1,15 @@
 class Ride
   attr_reader :name,
               :distance,
-              :terrain
+              :terrain,
+              :times
   
   def initialize(info)
     @info = info
     @name = info[:name]
     @distance = info[:distance]
     @terrain = info[:terrain]
+    @times = []
   end
 
   def loop?
@@ -20,5 +22,9 @@ class Ride
     else
       @distance * 2
     end
+  end
+
+  def record_time(time)
+    @times << time
   end
 end
