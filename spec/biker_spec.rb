4c951233697c1d9007ef25c0f77cba45ce01a7e6
Anna Wiley <awiley33@gmail.com>
2023-06-05 10:28:37 -0500

    test: Add before block and exists test for Biker class

--- spec/biker_spec.rb
+++ spec/biker_spec.rb
@@ -1,1 +1,13 @@
 require 'spec_helper'
+
+describe Biker do
+  before(:each) do
+    @biker = Biker.new("Kenny", 30)
+  end
+
+  it "exists and has readable attributes" do
+    expect(@biker).to be_a Biker
+    expect(@biker.name).to eq("Kenny")
+    expect(@biker.max_distance).to eq(30)
+  end
+end
