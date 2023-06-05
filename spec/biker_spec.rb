d163104b712c82d77831dac3a669f795bc14c3be
Anna Wiley <awiley33@gmail.com>
2023-06-05 10:54:29 -0500

    test: Add test for logging rides

--- spec/biker_spec.rb
+++ spec/biker_spec.rb
@@ -3,6 +3,8 @@ require 'spec_helper'
 describe Biker do
   before(:each) do
     @biker = Biker.new("Kenny", 30)
+    @ride1 = Ride.new({name: "Walnut Creek Trail", distance: 10.7, loop: false, terrain: :hills})
+    @ride2 = Ride.new({name: "Town Lake", distance: 14.9, loop: true, terrain: :gravel})
   end
 
   it "exists and has readable attributes" do
@@ -18,5 +20,17 @@ describe Biker do
     @biker.learn_terrain!(:hills)
 
     expect(@biker.acceptable_terrain).to eq([:gravel, :hills])
+  end
+
+  it "starts with an empty rides array and can log rides" do
+    @biker.log_ride(@ride1, 92.5)
+    @biker.log_ride(@ride1, 91.1)
+    @biker.log_ride(@ride2, 60.9)
+    @biker.log_ride(@ride2, 61.6)
+
+    expect(@biker.rides).to eq({
+      @ride1 => [92.5, 91.1],
+      @ride2 => [60.9, 61.6]
+    })
   end
 end
