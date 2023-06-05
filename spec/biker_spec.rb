f3d059262cc80dcc6bec55c5bf6329457d0b4a49
Anna Wiley <awiley33@gmail.com>
2023-06-05 11:29:03 -0500

    test: Add test to check ride is within bikers max distance

--- spec/biker_spec.rb
+++ spec/biker_spec.rb
@@ -24,6 +24,8 @@ describe Biker do
   end
 
   it "starts with an empty rides array and can log rides" do
+    @biker.learn_terrain!(:gravel)
+    @biker.learn_terrain!(:hills)
     @biker.log_ride(@ride1, 92.5)
     @biker.log_ride(@ride1, 91.1)
     @biker.log_ride(@ride2, 60.9)
@@ -36,6 +38,8 @@ describe Biker do
   end
 
   it "can determine a biker's personal record for a specific ride" do
+    @biker.learn_terrain!(:gravel)
+    @biker.learn_terrain!(:hills)
     @biker.log_ride(@ride1, 92.5)
     @biker.log_ride(@ride1, 91.1)
     @biker.log_ride(@ride2, 60.9)
@@ -45,7 +49,23 @@ describe Biker do
     expect(@biker.personal_record(@ride2)).to eq(60.9)
   end
 
-  # it "cannot log rides for terrains the biker has not yet learned" do
+  it "cannot log rides for terrains the biker has not yet learned" do
+    @biker2.log_ride(@ride1, 97.0)
+    @biker2.log_ride(@ride2, 67.0)
 
-  # end
+    expect(@biker2.rides).to eq({})
+  end
+
+  it "cannot log rides with a distance exceeding biker's max distance" do
+    @biker2.learn_terrain!(:gravel)
+    @biker2.learn_terrain!(:hills)
+    @biker2.log_ride(@ride1, 95.0)
+    @biker2.log_ride(@ride2, 65.0)
+
+    expect(@biker2.rides).to eq({@ride2 => [65.0]})
+  end
+
+  it "will return false for personal records of rides it has not logged" do
+
+  end
 end
