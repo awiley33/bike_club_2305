8b3d365ce7987bffaa89542d8a635d07348a7c3d
Anna Wiley <awiley33@gmail.com>
2023-06-05 11:48:58 -0500

    test: Test for adding bikers to bike club

--- spec/bikeclub_spec.rb
+++ spec/bikeclub_spec.rb
@@ -3,6 +3,8 @@ require 'spec_helper'
 describe BikeClub do
   before(:each) do
     @club1 = BikeClub.new("Cali Cruisers")
+    @biker1 = Biker.new("Kenny", 30)
+    @biker2 = Biker.new("Athena", 15)
   end
 
   it "exists and has a name" do
@@ -10,4 +12,12 @@ describe BikeClub do
     expect(@club1.name).to eq("Cali Cruisers")
   end
 
+  it "starts with an empty list of bikers and can add bikers to it" do
+    expect(@club1.bikers).to eq([])
+
+    @club1.add_biker(@biker1)
+    @club1.add_biker(@biker2)
+
+    expect(@club1.bikers).to eq([@biker1, @biker2])
+  end
 end
